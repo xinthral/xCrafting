dce7847427ddc62c5ebf5a2580a6b05053a3b4cf
jjhassel <jjhassel@mtu.edu>
2024-01-10 09:52:12 -0500

    Remake breakout

---
+++ cli/cli.h
@@ -0,0 +1,9 @@
+#ifndef XCRAFTING_CLI_H
+#define XCRAFTING_CLI_H
+
+#include "../core/cookbook.h"
+#include "../core/ingredient.h"
+#include "../core/recipe.h"
+#include <stdio.h>
+
+#endif // XCRAFTING_CLI_H //
--- core/cli.h
+++
@@ -1,9 +0,0 @@
-#ifndef XCRAFTING_H
-#define XCRAFTING_H
-
-#include "cookbook.h"
-#include "ingredient.h"
-#include "recipe.h"
-#include <stdio.h>
-
-#endif // XCRAFTING_H //
